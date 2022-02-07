d8c3378cd2158263a873e482bf06043e79cbcb8e
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-02-06 22:40:49 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit',(e) =>{
+    e.preventDefault()
+    let userInput = e.target.new_task_description.value
+    buildList(userInput);
+    form.reset()
+  })
 });
+
+function buildList(item) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  li.innerText = item
+  btn.innerText = ' Delete'
+  btn.addEventListener('click', (e) =>{
+    e.target.parentNode.remove()
+  })
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
